f368fc644c4c4e35fb6e9fdc8f1d2d217649dac6
Denis Ignatev <godis7.15@gmail.com>
2018-10-05 23:58:23 +0300

    Auto stash before merge of "master" and "origin/master"

---
+++ MagicCalculator/dynamicbuttons/src/main/java/com/example/dynamicbuttons/CompileProgram.java
@@ -0,0 +1,59 @@
+package com.example.dynamicbuttons;
+
+import java.util.Map;
+
+public class CompileProgram implements IFunction{
+
+    private Calculator calculator;
+    private String program;
+    private Map<String, IFunction> functions;
+    private Map<String, Variable> variables;
+
+    private int paramsNum;
+
+    public CompileProgram(String _prog, Calculator _calc, Map<String, IFunction> _func)
+    {
+        calculator = _calc;
+        program = _prog;
+        functions = _func;
+        Compile();
+    }
+
+    private void Compile() {
+        
+    }
+
+    private interface IExpression {
+        IExpression Next();
+    };
+
+    private class OperationExpression implements IExpression{
+
+        private String expression;
+
+        @Override
+        public IExpression Next() {
+
+        }
+    };
+
+    @Override
+    public double Calculate(double[] params) {
+        return 0.0;
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
