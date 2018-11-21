6d4fce8625531e9fc7e5e3430b91614be418c2ea
Denis Ignatev <godis7.15@gmail.com>
2018-11-21 12:49:58 +0300

    Creating Button Dialog development

--- MagicCalculator/calculator/src/main/java/com/project/calculator/CreateButtonWindow.java
+++ MagicCalculator/calculator/src/main/java/com/project/calculator/CreateButtonWindow.java
@@ -4,38 +4,181 @@ import android.app.AlertDialog;
 import android.app.Dialog;
 import android.app.DialogFragment;
 import android.content.DialogInterface;
-import android.graphics.Color;
-import android.graphics.drawable.ColorDrawable;
-import android.os.Build;
+import android.graphics.drawable.Drawable;
 import android.os.Bundle;
-import android.support.annotation.ColorInt;
-import android.support.annotation.RequiresApi;
 import android.text.Editable;
 import android.text.TextWatcher;
+import android.util.AttributeSet;
 import android.view.View;
-import android.view.ViewGroup;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.LinearLayout;
 import android.widget.Spinner;
 import android.widget.TextView;
 
-import java.text.ParseException;
+import org.w3c.dom.Text;
 
 // this class is dialog for creating a program-button
 public class CreateButtonWindow extends DialogFragment {
 
-    private EditText text;
-    private EditText body;
-    private Spinner paramNum;
-    private UserProgramCompiler compiler;
-
+    private EditText functionNameView;
+    private EditText bodyTextView;
+    private Spinner paramNumView;
+    private View currActivityView;
     private TextWatcher textWatcher;
+
+    private String positiveButtonText = "Save";
+    private String negativeButtonText = "Cancel";
 
     public interface CreateButtonWindowListener {
         void onDialogPositiveClick(CreateButtonWindow dialog);
         void onDialogNegativeClick(CreateButtonWindow dialog);
+    }
+
+    private void ParamNumberViewInit() {
+        paramNumView = currActivityView.findViewById(R.id.func_param_num);
+        InitParamNumberSpinner(5);
+
+        paramNumView.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+
+                view.setBackgroundColor(getResources().getColor(R.color.yellowOrange));
+                ((TextView)view).setTextColor(getResources().getColor(R.color.absolutelyBlack));
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+
+            }
+        });
+
+    }
+
+    private void FunctionNameViewInit() {
+        functionNameView = currActivityView.findViewById(R.id.function_name);
+    }
+
+    private void BodyTextViewInit() {
+        bodyTextView = currActivityView.findViewById(R.id.function_body);
+
+        textWatcher = new TextWatcher() {
+            private int lastSize = 0;
+            private int currentSize = 0;
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+                currentSize = after;
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+                lastSize = before;
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+                int pos =  bodyTextView.getSelectionStart();
+                int shift =  TextCorrection(bodyTextView, s, currentSize > lastSize);
+
+                int newPos = (pos + shift < 0) ? 0 : pos + shift;
+
+                bodyTextView.setSelection(newPos);
+            }
+        };
+
+        bodyTextView.addTextChangedListener(textWatcher);
+    }
+
+    private boolean CheckFunctionNameNotEmpty() {
+        return functionNameView.length() != 0;
+    }
+
+    private boolean CheckBodyTextNotEmpty() {
+        return bodyTextView.length() != 0;
+    }
+
+    @Override
+    public Dialog onCreateDialog(Bundle savedInstanceState) {
+
+        // base initialization
+        currActivityView = getActivity().getLayoutInflater().inflate(R.layout.user_program_editor, null);
+
+        ParamNumberViewInit();
+        FunctionNameViewInit();
+        BodyTextViewInit();
+
+        AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
+
+        builder.setView(currActivityView);
+        builder.setMessage("Creating new function")
+                .setPositiveButton(positiveButtonText, null)
+                .setNegativeButton(negativeButtonText, new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+
+                        CreateButtonWindowListener activity;
+                        try {
+                            activity = (CreateButtonWindowListener) getActivity();
+                            activity.onDialogNegativeClick(CreateButtonWindow.this);
+                        } catch (ClassCastException e) {
+                            throw new ClassCastException(getActivity().toString()
+                                    + " must implement CreateButtonWindowListener");
+                        }
+                    }
+                });
+
+        Dialog dialog = builder.create();
+        dialog.setOnShowListener(new DialogInterface.OnShowListener() {
+            @Override
+            public void onShow(DialogInterface dialog) {
+
+                int blackColor = getResources().getColor(R.color.absolutelyBlack);
+                int whiteColor = getResources().getColor(R.color.darkGray);
+
+                Button positiveButton = ((AlertDialog) dialog).getButton(AlertDialog.BUTTON_POSITIVE);
+                Button negativeButton = ((AlertDialog) dialog).getButton(AlertDialog.BUTTON_NEGATIVE);
+
+                positiveButton.setBackgroundColor(blackColor);
+                positiveButton.setTextColor(whiteColor);
+
+                negativeButton.setBackgroundColor(blackColor);
+                negativeButton.setTextColor(whiteColor);
+
+                positiveButton.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+
+                        if (!CheckFunctionNameNotEmpty()) {
+                            ShowErrorMessage("Enter function name");
+                            return;
+                        }
+
+                        CreateButtonWindowListener activity;
+
+                        try {
+                            activity = (CreateButtonWindowListener) getActivity();
+                            // checking if the code compiles
+                            // if it does, then close the window
+                            // else - put the error message in new window (in catch)
+                            activity.onDialogPositiveClick(CreateButtonWindow.this);
+                            getDialog().dismiss();
+
+                        } catch (ClassCastException e) {
+                            throw new ClassCastException(getActivity().toString()
+                                    + " must implement CreateButtonWindowListener");
+                        } catch (Error err) {
+                            ShowErrorMessage(err.getMessage());
+                        } catch (android.net.ParseException e) {
+                            ShowErrorMessage("Incorrect number of parameters");
+                        }
+                    }
+                });
+            }
+        });
+
+        return dialog;
     }
 
     private int TextCorrection(EditText textBox, Editable s, boolean isIncreased) {
@@ -120,140 +263,21 @@ public class CreateButtonWindow extends DialogFragment {
             data[i] = Integer.toString(i + 1) + " parameter" + ((i != 0) ? "s" : "");
         }
         ArrayAdapter<String> adapter = new ArrayAdapter<String>(getActivity(), R.layout.param_number_text, data);
-        paramNum.setAdapter(adapter);
-        paramNum.setPrompt("Func param num");
-        paramNum.setSelection(paramNum.getFirstVisiblePosition());
-    }
-
-    @Override
-    public Dialog onCreateDialog(Bundle savedInstanceState) {
-        AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
-
-        View view = getActivity().getLayoutInflater().inflate(R.layout.user_program_editor, null);
-        text = view.findViewById(R.id.function_name);
-        body = view.findViewById(R.id.function_body);
-        paramNum = view.findViewById(R.id.func_param_num);
-
-        InitParamNumberSpinner(5);
-
-        paramNum.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
-            @Override
-            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
-
-                Button butCalc = getActivity().findViewById(R.id.buttoncalc);
-                ColorDrawable butColor = (ColorDrawable)butCalc.getBackground();
-                view.setBackgroundColor(butColor.getColor());
-
-                ((TextView)view).setTextColor(Color.BLACK);
-            }
-
-            @Override
-            public void onNothingSelected(AdapterView<?> parent) {
-
-            }
-        });
-
-        textWatcher = new TextWatcher() {
-            private int lastSize = 0;
-            private int currentSize = 0;
-            @Override
-            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
-                currentSize = after;
-            }
-
-            @Override
-            public void onTextChanged(CharSequence s, int start, int before, int count) {
-                lastSize = before;
-            }
-
-            @Override
-            public void afterTextChanged(Editable s) {
-                int pos =  body.getSelectionStart();
-                int shift =  TextCorrection(body, s, currentSize > lastSize);
-
-                int newPos = (pos + shift < 0) ? 0 : pos + shift;
-
-                body.setSelection(newPos);
-            }
-        };
-
-        body.addTextChangedListener(textWatcher);
-
-        builder.setView(view);
-
-        builder.setMessage("Enter button name")
-                // we'll add the pos click listener below
-                // now it's null
-        .setPositiveButton("OK", null)
-        .setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
-            @Override
-            public void onClick(DialogInterface dialog, int which) {
-                CreateButtonWindowListener activity;
-                try{
-                    activity = (CreateButtonWindowListener) getActivity();
-                    activity.onDialogNegativeClick(CreateButtonWindow.this);
-                }
-                catch(ClassCastException e) {
-                    throw new ClassCastException(getActivity().toString()
-                            + " must implement CreateButtonWindowListener");
-                }
-            }
-        });
-
-        Dialog dialog = builder.create();
-        dialog.setOnShowListener(new DialogInterface.OnShowListener() {
-            @Override
-            public void onShow(DialogInterface dialog) {
-                Button posButton = ((AlertDialog) dialog).getButton(AlertDialog.BUTTON_POSITIVE);
-                final EditText editText = ((AlertDialog) dialog).findViewById(R.id.function_name);
-
-                //
-                posButton.setOnClickListener(new View.OnClickListener() {
-                    @Override
-                    public void onClick(View v) {
-
-                        CreateButtonWindowListener activity;
-                        if (editText.getText().length() == 0) {
-                            ShowErrorMessage("Enter function name");
-                            return;
-                        }
-
-                        try {
-                            activity = (CreateButtonWindowListener) getActivity();
-
-                            // checking if the code compiles
-                            // if it does, then close the window
-                            // else - put the error message in new window (in catch)
-                            activity.onDialogPositiveClick(CreateButtonWindow.this);
-                            getDialog().dismiss();
-
-                        } catch (ClassCastException e) {
-                            throw new ClassCastException(getActivity().toString()
-                                    + " must implement CreateButtonWindowListener");
-                        } catch (Error err) {
-                            ShowErrorMessage(err.getMessage());
-                        } catch (android.net.ParseException e) {
-                            ShowErrorMessage("Incorrect number of parameters");
-                        }
-                    }
-                });
-            }
-        });
-
-        return dialog;
+        paramNumView.setAdapter(adapter);
+        paramNumView.setPrompt("Func param num");
+        paramNumView.setSelection(paramNumView.getFirstVisiblePosition());
     }
 
     public String GetText() {
-        return text.getText().toString();
+        return functionNameView.getText().toString();
     }
 
-
     public String GetBody() {
-        return body.getText().toString();
+        return bodyTextView.getText().toString();
     }
 
     public int GetParamNum() {
-        int pNum = paramNum.getSelectedItemPosition() + 1;
+        int pNum = paramNumView.getSelectedItemPosition() + 1;
         return pNum;
     }
 
